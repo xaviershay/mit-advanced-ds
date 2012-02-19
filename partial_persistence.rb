97533d2769233e9dcf667ea35add8295f382f181
Xavier Shay <xavier@rhnh.net>
2012-02-19 14:24:03 -0800

    Editing

--- partial_persistence.rb
+++ partial_persistence.rb
@@ -2,17 +2,19 @@
 # kept, so that old versions can be queried. Partial persistence is a form of
 # this where updates are only permitted on the latest version.
 #
-# The structure implemented here was described in lecture one of the "Advanced
-# Data Structures" class at MIT, [available
+# The structure implemented here supports partial persistence with amortized
+# constant time performance (i.e. average `O(1)`) for both queries and updates,
+# which is pretty neat.  It was described in lecture one of the "Advanced Data
+# Structures" class at MIT, [available
 # online](http://courses.csail.mit.edu/6.851/spring12/lectures/L01.html).
 
-# Minitest is used both to document and verify the behaviour of the structure.
+# Minitest is used both to document and verify behaviour.
 require 'minitest/autorun'
 
 class PartialPersistenceTest < MiniTest::Unit::TestCase
   # This code demonstrates partial persistence of a record-based data structure.
   # Each element in the record is either a value or a pointer to another record.
-  # Updates are performed by specifiying a path to the record, an index into
+  # Updates are performed by specifying a path to the record, an index into
   # that record, and the new value. Querying is done by inspecting the entire
   # structure at a point in time.
   def test_interface
@@ -24,16 +26,22 @@ class PartialPersistenceTest < MiniTest::Unit::TestCase
     assert_equal [8, 2, [3, 9]], ds.unwrap(2)
   end
 
-  # Both updates and queries can be performed in constant time using this
-  # structure, which is pretty neat. This will be explained further below.
   def test_performance
     ds = PartialPersistence.wrap([1, 2, [3, -1]])
-    (1..100).each do |x|
+    range = (1..101)
+    range.each do |x|
       ds.set([2], 1, x)
     end
-    (1..100).each do |x|
+
+    range.each do |x|
       assert_equal [1, 2, [3, x]], ds.unwrap(x)
     end
+    Visualizer.output(ds)
+  end
+
+  # The theory holds for cyclic data structures, but this implementation does
+  # not yet support them.
+  def test_cyclic
   end
 end
 
@@ -49,27 +57,29 @@ class Record
   attr_reader :values
 
   # Alongside the values, a list of deltas (modifications) is stored. The value
-  # of the record at time *t* is `values` with `deltas` applied up until *t*.
+  # of the record at time *t* is `values` with `deltas` applied to *t*.
   #
   # This is sufficient to achieve partial persistence, but requires `O(m)` time
-  # to query the current version since all modifications need to be applied
-  # from the beginning.
+  # to query the current version since all deltas need to be applied from the
+  # beginning of time.
   class Delta < Struct.new(:t, :index, :new_value)
     def inspect
-      "%s@T%i = %s" % [index, t, new_value]
+      "%i@T%i = %s" % [
+        index,
+        t,
+        new_value.is_a?(Record) ? "Record" : new_value
+      ]
     end
   end
   attr_reader :deltas
 
-  # To achieve amortized constant (i.e. average `O(1)`) time the structure
-  # needs to be rebalanced periodically, which may involve creating new
-  # records. For this to work, all links between records are stored
-  # bi-directionally so that they can be easily updated when a new record is
-  # created. The forward link is already stored in `values`, here a separate
-  # list of the back links is stored.  If record `A` has record `B` as a value
-  # at `values[1]`, then `B` will store a back link `(A, 1)`.
-  #
-  # The utility of this will be further explained later.
+  # To achieve amortized constant time the structure needs to be rebalanced
+  # periodically, which may involve creating new records. For this to work, all
+  # links between records are stored bi-directionally so that they can be
+  # quickly updated when a new record is created. The forward link is already
+  # stored in `values`, here a separate list of the back links is stored.  If
+  # record `A` has record `B` as a value at `values[1]`, then `B` will store a
+  # back link `(A, 1)`.
   class Backlink < Struct.new(:record, :index); end
 
   def add_backlink(record, index)
@@ -95,9 +105,11 @@ end
 # ### Root
 #
 # A special root node is required to act as a constant entry point for the
-# structure. Since records can normally be rebalanced and new records created,
-# without this entry point there is no way to know which record to start with
-# for a given version! Comparatively, it has a naive implementation.
+# structure. Since records can be rebalanced and new records created, without
+# this entry point there is no way to know which record to start with for a
+# given version! An efficient implementation of the root node is outside the
+# scope of what is being demonstrated. A naive but easy to understand
+# implementation is provided so as not to distract.
 class Root
   attr_reader :records
 
@@ -150,9 +162,7 @@ end
 
 # A special case for handling deltas is required for the root node, otherwise
 # there is no way to get get a handle on an initial node for any given
-# version.  This is not normally included in the constant amortized time
-# calculation, I don't know how to to it better than `O(log n)` using a
-# binary search tree. (Current implementation is naive, does not use tree.)
+# version.
 class Root
   def value_at_time(t)
     records.to_a.reverse.detect {|tn, _| # TODO: Optimize
@@ -182,8 +192,8 @@ end
 
 # ### Updates
 #
-# Updating the structure first requires querying the current version of the
-# structure to extract the record referenced by the given `indices`.
+# Updating first requires querying the current version of the structure to
+# extract the record referenced by the given `indices`.
 class PartialPersistence
   def set(path, index, value)
     current_root   = root.value_at_time(now)
@@ -209,9 +219,7 @@ private
 end
 
 class Record
-  MAX_DELTAS = 5 # TODO: MOve and document
-
-  # We have now reached the heart of the algorithm. When the number of deltas
+  # This is the heart of the algorithm. When the number of deltas
   # crosses a threshold, the structure is rebalanced by performing the
   # following operations:
   #
@@ -239,6 +247,7 @@ class Record
     else
       new_record = rebalance(t)
       copy_backlinks(new_record, t)
+      clear_backlinks
       deltas.pop # TODO: Remove this
       new_record
     end
@@ -250,18 +259,38 @@ private
     self.class.new(values_at_time(t))
   end
 
+  # A special case is required when adding a back link to a parent that has
+  # itself just rebalanced. In this case, the parent has already added itself
+  # as a back link to the record as part of the creation process, so it should
+  # not be added again here.
   def copy_backlinks(new_record, t)
     backlinks.each do |l|
-      new_record.add_backlink(
-        l.record.add_delta(t, l.index, new_record),
-        l.index
-      )
+      new_back_record   = l.record.add_delta(t, l.index, new_record)
+      parent_rebalanced = new_back_record != l.record
+
+      unless parent_rebalanced
+        new_record.add_backlink(
+          new_back_record,
+          l.index
+        )
+      end
     end
   end
+
+  # A node can only ever be rebalanced once, meaning that the back links no
+  # longer need to be stored after it has happened.
+  def clear_backlinks
+    @backlinks = []
+  end
+
+  # A caveat is that the amortized constant time bound only holds when the
+  # maximum number of back links to any one record is less than or equal to
+  # half the number of deltas stored per record.
+  MAX_DELTAS = 10
 end
 
-# The root stores deltas in a more traditional manner. See the "queries"
-# section for details on performance.
+# As mentioned above, a naive implementation is provided for storing deltas in
+# the root and is not relevant to the structure being presented.
 class Root
   def add_delta(t, index, value)
     raise unless index == 0
@@ -284,286 +313,83 @@ def PartialPersistence.wrap(array)
   new(wrapper[array])
 end
 
-# ## Old Code
-if false
-$nodes = []
-class PartiallyPersistentArray
-  attr_reader :current_version
+# ## Debugging
+#
+# ### Labelings
 
-  def self.wrap(array)
-    new(Node.new(array.map {|x|
-      if x.is_a?(Array)
-        Node.new(x)
-      else
-        x
-      end
-    }))
-  end
+class Delta
+end
 
-  def inspect
-    "<PartiallyPersistentArray #{@root.inspect}>"
-  end
+# ### Visualization
+#
+# Programatically creating a diagram of the structure with GraphViz is straight
+# forward, yet finicky.
 
-  def output(filename = 'output.png')
+class Visualizer
+  def self.output(ds, filename = 'output.png')
     require 'graphviz'
 
     g = GraphViz::new("structs")
-    @root.to_graph(g)
+
+    add_root(g, ds.root)
+    ObjectSpace.each_object(Record) do |record|
+      add_record(g, record)
+    end
+
     g.output( :png => filename)
   end
 
-  def initialize(node)
-    @root = Root.new(node)
-    @current_version = 0
+  def self.add_root(g, root)
+    g.add_nodes(root.object_id.to_s,
+      "shape" => "record",
+      "label" => root.records.map {|(t, _)|
+        "<t%i> T%i" % [t, t]
+      }.join("|")
+    )
+
+    root.records.map {|(t, child)|
+      g.add_edges( {root.object_id.to_s => "t#{t}"}, child.object_id.to_s )
+    }
   end
 
-  def unwrap(t)
-    @root.unwrap(t) # Returns a node
+  def self.add_record(g, record)
+    values = record.values.map.with_index {|v, i| "<v%s> %s" % [i, label(v)] }
+    deltas = record.deltas.map.with_index {|d, i| "<d%s> %s" % [i, d.inspect] }
+
+    g.add_nodes(record.object_id.to_s,
+      "shape" => "record",
+      "label" => [
+        values.join("|"),
+        deltas.join("|")
+      ].map {|x| "{%s}" % x }.join("|")
+    )
+
+    add_edges(g, record, record.values, ->(x){ x }, "v")
+    add_edges(g, record, record.deltas, ->(x){ x.new_value }, "d")
+    add_edges(g, record, record.backlinks, ->(x){ x.record }, "",
+      style:     'dotted',
+      arrowhead: 'empty'
+    )
   end
 
-  def set(indexes, value)
-    root = @root.get(current_version)
-    indexes = [*indexes]
+  def self.add_edges(g, parent, children, unwrap, source_prefix, opts = {})
+    children.each.with_index do |child, i|
+      child = unwrap[child]
+      next unless child.is_a?(Record) || child.is_a?(Root)
 
-    index = indexes.last
-    node = indexes[0..-2].inject(root) do |node, i|
-      node.slots_at_time(current_version)[i] # Could be optimized because only need one element of node
-    end
-
-    @current_version += 1
-
-    node.set(index, value, current_version)
-  end
-
-  def get(version = current_version)
-    @root.get(version).to_a
-  end
-
-private
-
-  class Root
-    def initialize(node)
-      @roots = {
-        0 => node
-      }
-      node.add_backlink(self, 0)
-    end
-
-    def unwrap(t)
-      get(t).slots_at_time(t).map {|x|
-        if x.is_a?(Node)
-          x.slots_at_time(t)
-        else
-          x
-        end
-      }
-    end
-
-    def set(index, value, version)
-      raise unless index == 0
-      @roots[version] = value
-      self
-    end
-
-    def get(version)
-      # TODO: optimize
-      @roots.to_a.reverse.detect {|v, node|
-        v <= version
-      }.last
-    end
-
-    def inspect
-      "<Root>" # #{@roots.inspect}>"
-    end
-    def number
-      0
-    end
-
-    def to_graph(g)
-      g.add_nodes(self.object_id.to_s,
-        "shape" => "record",
-        "label" => @roots.map.with_index {|(v, _), i|
-          "<f#{v}> #{v}"
-        }.join("|")
+      g.add_edges(
+        {parent.object_id.to_s => "#{source_prefix}#{i}"},
+        child.object_id.to_s,
+        opts
       )
-      all_nodes = []
-      @roots.each do |v, node|
-        node.add_to_graph(g, all_nodes)
-
-        # Add edges from t to nodes
-        g.add_edges( {self.object_id.to_s => "v#{v}"}, "#{node.object_id}" )
-      end
-
     end
   end
 
-  class Node
-    def add_to_graph(g, all_nodes)
-      # Add self
-      g.add_nodes(self.object_id.to_s,
-        "shape" => "record",
-        "label" => [
-          @slots.map.with_index {|x, i| "<s#{i}> #{x.is_a?(Node) ? "Node" : x}"}.join("|"),
-          @modifications.map {|(t, idx, value)| "<m#{t}> #{idx} @ T#{t} = #{value.is_a?(Node) ? "Node" : value}"}.join("|")
-        ].map {|x| "{#{x}}" }.join("|").tap {|y| puts y })
-
-      # Add node slots
-      @slots.each.with_index do |node, s|
-        next unless node.is_a?(Node)
-
-        node.add_to_graph(g, all_nodes)
-
-        # Add edges from slots to nodes
-        g.add_edges( {self.object_id.to_s => "s#{s}"}, "#{node.object_id}" )
-      end
-
-      # Add edges from mods
-      @modifications.each {|(t, idx, node)|
-        next unless node.is_a?(Node)
-
-        node.add_to_graph(g, all_nodes)
-
-        # Add edges from mods to nodes
-        g.add_edges( {self.object_id.to_s => "m#{t}"}, "#{node.object_id}" )
-      }
-
-      @backlinks.each {|(node, idx)|
-        g.add_edges( self.object_id.to_s, "#{node.object_id}" )
-      }
-    end
-
-    def initialize(slots, max_mods = 4)
-      $nodes << self
-      @number = $nodes.length
-      @slots         = slots
-      @modifications = []
-      @max_mods      = max_mods
-      @backlinks     = []
-
-      @slots.each.with_index do |x, i|
-        if x.is_a?(Node)
-          x.add_backlink(self, i)
-        end
-      end
-    end
-
-    def add_backlink(node, index)
-      @backlinks << [node, index]
-    end
-
-    def set(index, value, version)
-      @modifications << [version, index, value]
-      @modifications.uniq!
-
-      if @modifications.length < @max_mods
-        self
-      else
-        puts "REBALANCING @ #{version}: #{@modifications.map(&:first)}"
-        new_node = rebalance(version)
-        @backlinks.each do |(node, backlink_index)|
-            back_node = node.set(backlink_index, new_node, version)
-        puts "BACK: " + back_node.inspect
-          new_node.add_backlink(
-            back_node,
-            backlink_index
-          )
-        end
-        puts "NEW: " + new_node.inspect
-        @modifications.pop
-        new_node
-      end
-    end
-
-    def slots_at_time(t)
-      base = @slots.dup
-      @modifications.each do |(v, i, x)|
-        break if v > t
-        base[i] = x
-      end
-
-      base
-    end
-
-    attr_reader :number
-    def inspect
-      "<Node:#{number} " +
-      "#{@slots.map {|x| x.is_a?(Node) ? "N#{x.number}" : x }}" +
-        " mods=#{@modifications.map {|(t, idx, value)| "#{idx}@#{t} #{value.is_a?(Node) ? "N#{value.number}" : value}" }} backlinks=#{@backlinks.map(&:first).map(&:number)}>"
-    end
-
-  private
-
-    def rebalance(version)
-      Node.new(slots_at_time(version))
+  def self.label(v)
+    if v.is_a?(Record)
+      "Record"
+    else
+      v
     end
   end
-end
-
-describe 'partial persistence' do
-  it 'round trips an array' do
-    ds = PartiallyPersistentArray.wrap([1, 2])
-    ds.unwrap(0).should == [1, 2]
-  end
-
-  it 'updates a value in the array' do
-    ds = PartiallyPersistentArray.wrap([1, 2])
-    ds.set([1], 3)
-    ds.unwrap(0).should == [1, 2]
-    ds.unwrap(1).should == [1, 3]
-  end
-
-  it 'supports nesting' do
-    ds = PartiallyPersistentArray.wrap([1, [2, 3]])
-    ds.set([1, 1], 4)
-    ds.set([1, 0], 5)
-    ds.set([0], 7)
-    ds.set([1, 0], 8)
-    ds.unwrap(0).should == [1, [2, 3]]
-    ds.unwrap(1).should == [1, [2, 4]]
-    ds.unwrap(2).should == [1, [5, 4]]
-  end
-
-  it 'supports cyclic arrays' do
-    pending
-    b = []
-    a = [1, b]
-    b << a
-    ds = PartiallyPersistentArray.wrap(a)
-    ds.set([1, 1, 1, 1, 1, 0], 2)
-    ds.unwrap(0).should == a
-    ds.unwrap(1).should == [2, b]
-  end
-
-  it 'rebalances the root' do
-    ds = PartiallyPersistentArray.wrap([0])
-    (1..100).each do |x|
-      ds.set([0], x)
-    end
-    (0..100).each do |x|
-      ds.unwrap(x).should == [x]
-    end
-  end
-
-  it 'rebalances nested arrays' do
-    ds = PartiallyPersistentArray.wrap([[0], "abc"])
-    begin
-    puts ds.inspect
-    (1..25).each do |x|
-      ds.set([0, 0], x)
-    $nodes.each do |node|
-      puts node.inspect
-    end
-    puts
-    end
-#     (0..25).each do |x|
-#       ds.unwrap(x).should == [[x]]
-#     end
-    ensure
-    $nodes.each do |node|
-      puts node.inspect
-    end
-    ds.output
-    end
-  end
-end
 end
