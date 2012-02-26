1162c0691cee7d56044b9810367c5c41b1cf2f66
Xavier Shay <xavier@squareup.com>
2012-02-25 16:51:18 -0800

    Simple binary tree.

---
+++ binary_tree.rb
@@ -0,0 +1,83 @@
+require 'minitest/autorun'
+require 'minitest/benchmark'
+
+class TreeTest < MiniTest::Unit::TestCase
+  def test_simple
+    set = BinaryTree.new
+    set.insert(7)
+    set.insert(3)
+    set.insert(9)
+    assert  set.include?(3)
+    assert !set.include?(8)
+  end
+
+  def test_quickcheck
+    set = BinaryTree.new
+    range = (0..100).to_a
+    range.sort_by { rand }.each do |x|
+      set.insert(x)
+    end
+
+    range.each do |x|
+      assert set.include?(x), "Did not include #{x}"
+    end
+    assert !set.include?(-1)
+    assert !set.include?(101)
+  end
+end
+
+class BinaryTree
+  class Node < Struct.new(:value, :left, :right)
+    def empty?; !value; end
+    def inspect
+      return '-' if empty?
+
+      buffer = "<#{value} #{left.inspect} #{right.inspect}>"
+    end
+
+    def insert(x)
+      return if x == value
+
+      if empty?
+        self.value = x
+        self.left  = Node.empty
+        self.right = Node.empty
+      else
+        if x < value
+          left.insert(x)
+        else
+          right.insert(x)
+        end
+      end
+    end
+
+    def include?(x)
+      return false if empty?
+      return true  if x == value
+
+      if x < value
+        left.include?(x)
+      else
+        right.include?(x)
+      end
+    end
+
+    def self.empty
+      new(nil)
+    end
+  end
+
+  attr_accessor :root
+
+  def initialize
+    @root = Node.empty
+  end
+
+  def insert(x)
+    root.insert(x)
+  end
+
+  def include?(x)
+    root.include?(x)
+  end
+end
