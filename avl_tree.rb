e3a62ca70a83d49504715230a230b2f296249797
Xavier Shay <xavier@squareup.com>
2012-03-04 12:54:53 -0800

    WIP

---
+++ avl_tree.rb
@@ -0,0 +1,135 @@
+require 'minitest/autorun'
+
+class AvlTreeTest < MiniTest::Unit::TestCase
+  def test_operations
+    1.times do
+      set   = AvlTree.new
+      range = (0..5).to_a
+      range.sort_by { rand }.each {|x| set.insert(x) }
+
+      keep   = range[0..range.length/2]
+      remove = range[range.length/2+1..-1]
+
+#       remove.sort_by { rand }.each {|x| set.delete(x) }
+
+      keep  .each { |x| assert  set.include?(x), "Did not include #{x}" }
+#       remove.each { |x| assert !set.include?(x), "Did include #{x}" }
+
+      draw(set, 'tree.png')
+    end
+  end
+end
+
+def draw(ds, filename)
+  require 'graphviz'
+
+  g = GraphViz::new("structs")
+
+  ds.each do |node|
+    p node
+    g.add_nodes(node.object_id.to_s,
+      "shape" => "record",
+      "label" => "%i (%i)" % [node.value.to_s, node.balance]
+    )
+
+    edge = lambda do |subtree|
+      unless subtree.empty?
+        g.add_edges(node.object_id.to_s, subtree.object_id.to_s)
+      end
+    end
+
+    edge[node.left]
+    edge[node.right]
+  end
+
+  g.output(:png => filename)
+end
+
+class AvlTree
+  class Node < Struct.new(:value, :parent, :left, :right, :balance)
+    EMPTY = Class.new
+
+    def self.empty(parent)
+      new(EMPTY, parent)
+    end
+
+    def empty?
+      value == EMPTY
+    end
+
+    def inspect
+      return '-' if empty?
+
+      buffer = "<#{value} #{balance} #{left.inspect} #{right.inspect}>"
+    end
+
+    def insert(x)
+      return if x == value
+
+      if empty?
+        self.value   = x
+        self.left    = Node.empty(self)
+        self.right   = Node.empty(self)
+        self.balance = 0
+      else
+        if x < value
+          if left.insert(x)
+            self.balance -= 1
+            parent.rebalance(-1)
+          end
+        else
+          if right.insert(x)
+            self.balance += 1
+            parent.rebalance(1)
+          end
+        end
+      end
+    end
+
+    def rebalance(d)
+      self.balance += d
+      self.parent.rebalance(d)
+    end
+
+    def include?(x)
+      return false if empty?
+      return true  if x == value
+
+      subtree = if x < value
+               left
+             else
+               right
+             end
+      subtree.include?(x)
+    end
+
+    def each(&block)
+      return if empty?
+
+      yield self
+      left.each(&block)
+      right.each(&block)
+    end
+  end
+
+  class PseudoRoot
+    def rebalance(d)
+    end
+  end
+
+  def initialize
+    @root = Node.empty(PseudoRoot.new)
+  end
+
+  def insert(x)
+    @root.insert(x)
+  end
+
+  def include?(x)
+    @root.include?(x)
+  end
+
+  def each(&block)
+    @root.each(&block)
+  end
+end
